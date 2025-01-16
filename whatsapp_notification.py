ed00da622b32bde9898d4053b42c6d9d1b3b9486
riansetyabudi <riansbudi19@gmail.com>
2025-01-16 21:29:05 +0700

    refactor : move twilio sid to env file

--- whatsapp_notification.py
+++ whatsapp_notification.py
@@ -1,7 +1,11 @@
+import os
 from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def send_whatsapp_notification(vehicle_count):
-    account_sid = 'AC9e351ef437cb18718fc924cc5aec6b1d'
+    account_sid = os.getenv('TWILIO_SID') 
     auth_token = '[AuthToken]'  # Ganti dengan Auth Token Anda
     client = Client(account_sid, auth_token)
 
